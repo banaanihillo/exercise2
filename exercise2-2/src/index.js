26c7ba0aa684fcb5169b7ae05c6b62867d06525b
banaanihillo <banaanihillo@protonmail.ch>
2020-03-17 17:31:01 +0200

    Commited exercise2-2 (Part 2 of Course v2)
    Added Total component to display number of all exercises

---
+++ exercise2-2/src/index.js
@@ -0,0 +1,104 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Part = (props) => {
+    const {courseDetails} = props
+
+    return (
+        <div>
+            {courseDetails}
+        </div>
+    )    
+}
+
+const Header = (props) => {
+    const {courseName} = props
+
+    return (
+        <div>
+            <h1> Description of the course: {courseName} </h1>
+        </div>
+    )
+}
+
+const Content = (props) => {
+    const {parts} = props
+
+    return (
+        <div>
+            <Part courseDetails = {parts.map(part =>
+                <li key = {part.id}>
+                    {part.name} contains {part.exercises} exercises.
+                </li>
+            )} />
+        </div>
+    )
+}
+
+const Total = (props) => {
+    const {numberOfExercises} = props
+    let sum = 0
+    for (let i = 0; i<numberOfExercises.length; i++){
+        sum = sum + numberOfExercises[i];
+    }
+
+    return (
+        <div>
+            <p> The course contains a total of {sum} exercises. </p>
+        </div>
+    )
+}
+
+const Course = (props) => {
+    const {courseName, parts} = props
+
+    return (
+        <div>
+            <Header courseName = {courseName} />
+            <Content parts = {parts} />
+            <Total numberOfExercises = {parts.map(part => part.exercises)}/>
+        </div>
+    )
+}
+
+const App = () => {
+    const course = {
+        name: "Stack Half-Full",
+        id: 1,
+        parts: [
+            {
+                name: "Fundamentals of React",
+                exercises: 10,
+                id: 1
+            },
+            {
+                name: "Using props to pass data",
+                exercises: 7,
+                id: 2
+            },
+            {
+                name: "State of a component",
+                exercises: 14,
+                id: 3
+            },
+            {
+                name: "Nonexistent part of the course",
+                exercises: 0,
+                id: 4
+            },
+            {
+                name: "Redux",
+                exercises: 11,
+                id: 5
+            }
+        ]
+    }
+
+    return (
+        <div>
+            <Course courseName = {course.name} parts = {course.parts} />
+        </div>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
